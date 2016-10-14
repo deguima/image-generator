d0edbaf89c76a876e5cae64eae3329bf974bde84
deguima <you@example.com>
2016-10-13 23:09:53 -0400

    add line breaks to fillText

--- scripts.js
+++ scripts.js
@@ -1,21 +1,28 @@
 function wrapText(context, text, x, y, maxWidth, lineHeight) {
-  var words = text.split(' ');
-  var line = '';
+  var lines = text.split("<br/>");
 
-  for (var n = 0; n < words.length; n++) {
-    var testLine = line + words[n] + ' ';
-    var metrics = context.measureText(testLine);
-    var testWidth = metrics.width;
-    if (testWidth > maxWidth && n > 0) {
-      context.fillText(line, x, y);
-      line = words[n] + ' ';
-      y += lineHeight
+    for (var i = 0; i < lines.length; i++) {
+
+        var words = lines[i].split(' ');
+        var line = '';
+
+        for (var n = 0; n < words.length; n++) {
+            var testLine = line + words[n] + ' ';
+            var metrics = context.measureText(testLine);
+            var testWidth = metrics.width;
+            if (testWidth > maxWidth && n > 0) {
+                context.fillText(line, x, y);
+                line = words[n] + ' ';
+                y += lineHeight;
+            }
+            else {
+                line = testLine;
+            }
+        }
+
+        context.fillText(line, x, y);
+        y += lineHeight;
     }
-    else {
-      line = testLine;
-    }
-  }
-  context.fillText(line, x, y);
 }
 
 var canvas = document.getElementById('myCanvas');
@@ -24,14 +31,15 @@ var maxWidth = 400;
 var lineHeight = 25;
 var x = (canvas.width - maxWidth) / 2
 var y = 60;
-var text = 'All the world \'s a stage, and all the men and women merely players. They have their exits and their entrances; And one man in his time plays many parts.';
+var text = 'All the world \'s<br/>a stage, and all the men and women merely players. They have their exits and their entrances; And one man in his time plays many parts.';
 
 context.font = '16pt Calibri';
 context.fillStyle = '#333';
 
-
+ //wrapText(context,text, x, y, maxWidth, lineHeight);
 
 document.querySelector(".js-set-txt").addEventListener("keyup", function(){
-   // wrapText(context, this.value.replace(/\r?\n/g, '<br />'), x, y, maxWidth, lineHeight);
+    wrapText(context, this.value , x, y, maxWidth, lineHeight);
+    //wrapText(context,text, x, y, maxWidth, lineHeight);
     //console.log(this.value);
 });
